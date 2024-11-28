a903248712fffdf790003a6c4f201850ff212349
Sotatek-ManhTran2 <manh.tran2@sotatek.com>
2024-11-28 18:29:39 +0700

    update

--- contracts/lib/Address.sol
+++ contracts/lib/Address.sol
@@ -2,8 +2,55 @@
 pragma solidity 0.8.20;
 
 library Address {
-  function sendValue() internal {}
-  function staticCall() internal {}
-  function call() internal {}
-  function delegateCall() internal {}
+  error StaticCallFailed();
+  error CallFailed();
+  error DelegateCallFailed();
+  error InsufficientBalance();
+  error SendValueFailed();
+
+  function staticCall(
+    address target,
+    bytes memory data
+  ) internal view returns (bytes memory) {
+    (bool success, bytes memory result) = target.staticcall(data);
+    if (success) {
+      return result;
+    } else {
+      revert StaticCallFailed();
+    }
+  }
+
+  function sendValue(address recipient, uint256 value) internal {
+    if (address(this).balance < value) {
+      revert InsufficientBalance();
+    }
+    (bool success, ) = recipient.call{value: value}("");
+    if (!success) {
+      revert SendValueFailed();
+    }
+  }
+
+  function call(
+    address target,
+    bytes memory data
+  ) internal returns (bytes memory) {
+    (bool success, bytes memory result) = target.call(data);
+    if (success) {
+      return result;
+    } else {
+      revert CallFailed();
+    }
+  }
+
+  function delegateCall(
+    address target,
+    bytes memory data
+  ) internal returns (bytes memory) {
+    (bool success, bytes memory result) = target.delegatecall(data);
+    if (success) {
+      return result;
+    } else {
+      revert DelegateCallFailed();
+    }
+  }
 }
---
+++ contracts/utils/security/Pausable.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+
+abstract contract Pausable is AccessControl {
+  error OnlyOperator();
+
+  event Paused(address account);
+  event Unpaused(address account);
+
+  bool private _paused;
+  bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+
+  modifier onlyOperator() {
+    if (!hasRole(OPERATOR_ROLE, msg.sender)) {
+      revert OnlyOperator();
+    }
+    _;
+  }
+
+  constructor(address adminRole) {
+    _paused = false;
+    _grantRole(DEFAULT_ADMIN_ROLE, adminRole);
+  }
+
+  function _pause() internal virtual onlyOperator {
+    _paused = true;
+    emit Paused(msg.sender);
+  }
+
+  function _unpause() internal virtual onlyOperator {
+    _paused = false;
+    emit Unpaused(msg.sender);
+  }
+
+  function emergencyWithdraw() external virtual onlyOperator {
+    payable(msg.sender).transfer(address(this).balance);
+  }
+}
